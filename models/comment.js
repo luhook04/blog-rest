9880faa32700dd4233a187cafa3da9c337681558
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-07-31 16:30:03 -0400

    Create comment schema

---
+++ models/comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+
+const CommentSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    maxLength: 20,
+  },
+  text: {
+    type: String,
+    required: true,
+    maxLength: 300,
+  },
+  postId: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+CommentSchema.virtual("date").get(function () {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString(
+    DateTime.DATE_MED
+  );
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
--- models/post.js
+++ models/post.js
@@ -9,3 +9,5 @@ const AdminSchema = new Schema({
   },
   password: { type: String, required: true },
 });
+
+module.exports = mongoose.model("Admin", AdminSchema);
