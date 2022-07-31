95cef5e3be59bfdf08b8e85eff09d99eaccd6c55
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-07-31 17:04:48 -0400

    Setup a local strategy for authenticating a user in it's own file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+require("./config/passport");
 
 var indexRouter = require("./routes/index");
 
---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Admin = require("../models/admin");
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    Admin.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    });
+  })
+);
