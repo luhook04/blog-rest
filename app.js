efad16852dfbf772371f031dd1d1174cde87b53a
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-04 16:32:43 -0400

    Setup app to use compression, helmet, and cors

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 require("dotenv").config();
 require("./config/passport");
+
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 const mongoose = require("mongoose");
+
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
 
@@ -17,6 +22,9 @@ mongoose.connect(process.env.MONGO_DB_URI, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.use(helmet());
+app.use(compression());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
