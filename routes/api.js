e8eb70a2fc647cbc3029ba05a694b3460b0a806a
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-04 16:41:17 -0400

    Add jsonwebtoken authorization to the routes that need protection

--- routes/api.js
+++ routes/api.js
@@ -14,17 +14,27 @@ router.post("/login", admin_controller.login);
 
 router.get("/posts", post_controller.get_posts);
 router.get("/posts/:postId", post_controller.get_single_post);
-router.put("/posts/:postId", post_controller.update_post);
-router.delete("/posts/:postId", post_controller.delete_post);
+router.put(
+  "/posts/:postId",
+  passport.authenticate("jwt", { session: false }),
+  post_controller.update_post
+);
+router.delete(
+  "/posts/:postId",
+  passport.authenticate("jwt", { session: false }),
+  post_controller.delete_post
+);
 router.post(
   "/posts",
   passport.authenticate("jwt", { session: false }),
   post_controller.create_post
 );
+
 router.post("/posts/:postId/comments", comment_controller.create_comment);
 router.get("/posts/:postId/comments", comment_controller.get_comments);
 router.delete(
   "/posts/:postId/comments/:commentId",
+  passport.authenticate("jwt", { session: false }),
   comment_controller.delete_comment
 );
 
