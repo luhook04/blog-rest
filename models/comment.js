837c3d22a33e4ea5cf07c61a7a9d1b2e8e6da9c4
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-08-05 20:14:52 -0400

    Add ability for virtual properties to be passed as json

--- models/comment.js
+++ models/comment.js
@@ -2,20 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
 
-const CommentSchema = new Schema({
-  username: {
-    type: String,
-    require: true,
-    maxLength: 20,
+const CommentSchema = new Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      maxLength: 20,
+    },
+    text: {
+      type: String,
+      required: true,
+      maxLength: 300,
+    },
+    postId: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
   },
-  text: {
-    type: String,
-    required: true,
-    maxLength: 300,
-  },
-  postId: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 CommentSchema.virtual("date").get(function () {
   return DateTime.fromJSDate(this.timestamp).toLocaleString(
--- models/post.js
+++ models/post.js
@@ -2,17 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const { DateTime } = require("luxon");
 
-const PostSchema = new Schema({
-  title: { type: String, required: true },
-  text: {
-    type: String,
-    required: true,
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    text: {
+      type: String,
+      required: true,
+    },
+    authorName: { type: String, required: true },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    published: { type: Boolean, default: false },
+    timestamp: { type: Date, default: Date.now },
   },
-  authorName: { type: String, required: true },
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-  published: { type: Boolean, default: false },
-  timestamp: { type: Date, default: Date.now },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 PostSchema.virtual("date").get(function () {
   return DateTime.fromJSDate(this.timestamp).toLocaleString(
